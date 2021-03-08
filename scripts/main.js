b05027b1290f6c3eeb3ae3ccfdde2ba54adc4448
vladimir264 <vjcaso@gmail.com>
2021-03-08 09:17:22 -0500

    Update main.js
    Planning to add a slider to adjust the min and max zoom limits

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,32 @@
 //Le Zooom
 
-if(!Vars.headless){
-  Vars.renderer.minZoom = 0.5; //Zoom out farther
-  Vars.renderer.maxZoom = 25; //Get a closer look at yourself
+const defaultMinZoomLim = Vars.renderer.minZoom;
+const defaultMaxZoomLim = Vars.renderer.maxZoom;
+
+const minZoomLim = 0.5;
+const maxZoomLim = 25;
+
+// default extended zoom limits
+const minZoom = 0.75;
+const maxZoom = 20;
+
+function resetZoomLim(toOriginal){
+	if(!Vars.headless){
+		if(toOriginal){
+			Vars.renderer.minZoom = defaultMinZoomLim;
+	  	Vars.renderer.maxZoom = defaultMaxZoomLim;
+		} else {
+	  	Vars.renderer.minZoom = minZoomLim;
+	  	Vars.renderer.maxZoom = maxZoomLim;
+	  }
+	}
 }
+
+function updateZoom(min, max){
+	if(!Vars.headless){
+	  Vars.renderer.minZoom = min;
+	  Vars.renderer.maxZoom = max;
+	}
+}
+
+updateZoom(maxZoomLim,minZoomLim)
